c43789e64d7f4cd6dbf2bb74c3b9daaa5844955d
FunMelon <Fun_Melon@163.com>
2022-10-30 09:29:52 +0800

    solve the bug that lsp does not work.

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -18,10 +18,23 @@ require("mason-lspconfig").setup({
   },
 })
 
+local lspconfig = require("lspconfig")
+
 -- 安装列表
 -- { key: 语言 value: 配置文件 }
 -- key 必须为下列网址列出的名称
 -- https://github.com/williamboman/nvim-lsp-installer#available-lsps
 local servers = {
+  sumneko_lua = require("lsp.config.lua"), -- lua/lsp/config/lua.lua
   clangd = require("lsp.config.clangd"),
 }
+
+for name, config in pairs(servers) do
+  if config ~= nil and type(config) == "table" then
+    -- 自定义初始化配置文件必须实现on_setup 方法
+    config.on_setup(lspconfig[name])
+  else
+    -- 使用默认参数
+    lspconfig[name].setup({})
+  end
+end
